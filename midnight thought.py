bd03f42f86199713d19166952232f2f263f46281
ianoxplix <65483660+ianoxplix@users.noreply.github.com>
2024-07-12 00:36:14 +0300

    Update midnight thought.py

--- midnight thought.py
+++ midnight thought.py
@@ -1,1 +1,10 @@
 a script that takes data from a csv and pastes it on a browesr in details depending on the relevant field
+
+# plans changed ha !
+import pyautogui, time
+time.sleep(10)
+x = pyautogui.size()
+cllck_speed = 20000
+for _ in range(cllck_speed):
+    pyautogui.click()
+    
